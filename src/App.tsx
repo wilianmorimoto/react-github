7afbde945785a326025239d60ede47408bb5c25c
wilianmorimoto <wilianhmorimoto@gmail.com>
2024-04-28 13:30:37 -0300

    footer and themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
-import 'react-calendar-heatmap/dist/styles.css'
+import "react-calendar-heatmap/dist/styles.css";
 
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
-import Header from './components/Header'
-import Profile from './pages/Profile'
-import Repo from './pages/Repo'
+import Header from "./components/Header";
+import Profile from "./pages/Profile";
+import Repo from "./pages/Repo";
+import Footer from "./components/Footer";
 
-import GlobalStyles from './styles/GlobalStyles'
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
 	return (
-		<BrowserRouter>
-      <Header/>
-      
-      <Routes>
-        <Route path="/" element={<Profile />} />
-        <Route path="/:username" element={<Profile />} />
-        <Route path="/:username/:reponame" element={<Repo />} />
-      </Routes>
+		<ThemeProvider>
+			<BrowserRouter>
+				<Header />
 
-      {/* <Footer/> */}
-      <GlobalStyles />
-    </BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Profile />} />
+					<Route path="/:username" element={<Profile />} />
+					<Route path="/:username/:reponame" element={<Repo />} />
+				</Routes>
+
+				<Footer />
+
+				<GlobalStyles />
+			</BrowserRouter>
+		</ThemeProvider>
 	);
 }
 
